fed528c5c2f846d7a5efc9f72ec95ea8160534bd
potatodori <ehh740366@gmail.com>
2026-08-26 08:13:01 +0900

    refactor: apply device and result modules to TEST2 user&enable,fix init

--- TEST2_pwcombi_enable.py
+++ TEST2_pwcombi_enable.py
@@ -3,123 +3,18 @@
 ### This script is based on equipment of REALTAK Chipset ###  
 
 import time
-import re
-import mss
-import os
-from openpyxl import load_workbook
+import sys
 
+sys.path.append(r"C:\Git\script_security_project")
 
-# 결과 파일
-file_path = r"C:\Git\script_security_project\RESULT\securitytest_result.xlsx"
-save_path = file_path
-
-# 장비 정보
-device = "SWITCH"
-console = "/SERIAL COM6 /BAUD 115200"
-ip = "192.168.100.22"
-
-
-wb = load_workbook(file_path)
-sheet1 = wb["RESULT"]
-sheet2 = wb["LOG"]
-
-def get_next_row(sheet):
-    for row in range(sheet.max_row, 0, -1):
-        if any(cell.value for cell in sheet[row]):
-            return row +1
-    return 1
-
-
-###전체 line 읽기###
-all_lines = []
-
-def read_all():
-
-    global all_lines
-    all_lines = []
-
-    num_rows = 300
-    num_cols = 300
-
-    for row in range(1, num_rows + 1):
-        line = crt.Screen.Get(row, 1, row, num_cols).rstrip()
-        if line.strip() != "":
-            all_lines.append(line)
-
-
-
-###시험부분 line만 추출###
-def select(all_lines, TEST_start, TEST_end):
-
-    final_line = []
-    capture = False
-
-    for line in all_lines:
-        if TEST_start in line:
-            capture = True
-            final_line.append(line)
-            continue
-
-        if capture:
-            final_line.append(line)
-            if TEST_end in line:
-                break
-
-    final = "\n".join(final_line)
-
-    return final
-
-
-
-###판단 부분###
-def pw_verify(judge_count, find_line):
-
-    read_all()
-    final = select(all_lines, f"{device}# {test_name}_start", f"{device}# {test_name}_end")
-    j = 0
-
-    for judge in final.splitlines():
-        if find_line in judge:
-            j += 1
-
-    result = "PASS" if j == judge_count else "FAIL"
-
-    row1 = get_next_row(sheet1)
-    row2 = get_next_row(sheet2)
-
-    sheet1.cell(row=row1, column=2, value=test_name)
-    sheet1.cell(row=row1, column=3, value=result)
-    sheet1.cell(row=row1, column=4, value = j)
-
-    sheet2.cell(row=row2, column=2, value=test_name)
-    sheet2.cell(row=row2, column=3, value=final)
-    
-    wb.save(save_path)
-
-###장비 접속###
-def start():
-    crt.Session.Connect(console)
-    time.sleep(2)
-    crt.Screen.Send("\n")
-    crt.Screen.WaitForString("login")
-    crt.Screen.Send("admin\n")
-    crt.Screen.WaitForString("Password")
-    crt.Screen.Send("Changeme1357!\r")
-    crt.Screen.WaitForString(f"{device}>")
-    crt.Screen.Send("\n")
-    crt.Screen.Send("enable\n")
-    time.sleep(1)
-    
-###config mode 진입###
-def config_mode():
-
-    crt.Screen.Send("conf t\n")
-    crt.Screen.WaitForString(f"{device}(config)# ")
+import device
+import result
 
 
 ##########################################################################
+# TEST1 : 1.2.1 비밀번호 보안성 기준
+##########################################################################
 
-###TEST1 : 1.2.1 비밀번호 보안성 기준###
 combi_pw_list1 = [
         "024681357A",
         "ADGJLQETUa",
@@ -153,72 +48,151 @@ consecutive_pw_list2 = [
         "KTC4321!#"
     ]
 
-def make_user_admin2():
-    
-    config_mode()
-    crt.Screen.Send("username admin2 password admin\n")
-    #crt.Screen.Send("username admin2 password \n")
-    time.sleep(1)
-    crt.Screen.Send("Changeme1357!\r")
-    time.sleep(0.5)
-    crt.Screen.Send("Changeme1357!\r")
-    time.sleep(0.5)
-    crt.Screen.Send("end\n")
-    time.sleep(0.5)
-    crt.Screen.Send("show running-configuration |include admin2\n")
+
+##########################################################################
+# Enable Password 입력
+##########################################################################
 
 def make_enable():
 
     time.sleep(1)
     crt.Screen.Send("enable admin2 password\n")
-    #crt.Screen.Send("enable password\n")
-    time.sleep(1)
+    crt.Screen.WaitForString("Enable Password")
 
 
-def TEST1_PW_enable(pw_case):
+##########################################################################
+# Enable Password 조합 시험
+##########################################################################
+
+def TEST2_PW_enable(pw_case):
 
     crt.Screen.Send(f"{test_name}_start\n")
 
     time.sleep(1)
-    config_mode()
+    device.config_mode(crt)
 
     for pw in pw_case:
         make_enable()
+
         crt.Screen.Send(f"{pw}\r")
         time.sleep(0.3)
+
         crt.Screen.Send("\n")
-        crt.Screen.WaitForString(f"{device}(config)# ")    
+        crt.Screen.WaitForString(f"{device.device}(config)# ")    
 
     time.sleep(1)
-    crt.Screen.Send("end\n")
-    time.sleep(0.5)
+    device.end_config(crt)
+
     crt.Screen.Send("show running-configuration |include admin2\n")
     #crt.Screen.Send("show running-config |include enable password\n")
     
+    crt.Screen.WaitForString(f"{device.device}#")
     crt.Screen.Send(f"{test_name}_end\n")
     time.sleep(1)
 
 
-start()
-make_user_admin2()
+##########################################################################
+# 판정
+##########################################################################
 
-test_name = "TEST1_PW_combi_enable1"
-TEST1_PW_enable(combi_pw_list1)
+def pw_verify(judge_count, find_line):
+
+    final = result.test_part(
+        crt,
+        f"{device.device}# {test_name}_start",
+        f"{device.device}# {test_name}_end"
+    )
+
+    j=0
+
+    for judge in final.splitlines():
+
+        if find_line in judge:
+            j += 1
+
+    if j == judge_count:
+        test_result = "PASS"
+
+    else:
+        test_result = "FAIL"
+
+    judge_value = f"Count={j}, Expected={judge_count}"
+
+    result.save_result(
+        test_name,
+        test_result,
+        judge_value,
+        final
+    )
+
+
+##########################################################################
+# 시험 시작
+##########################################################################
+
+device.start(crt)
+
+##########################################################################
+# admin2 사용자 생성
+##########################################################################
+
+#device.create_user(crt, "admin2", "admin", "Changeme1357!")
+
+
+##########################################################################
+# 조합 미충족
+##########################################################################
+
+test_name = "TEST2_PW_combi_enable1"
+TEST2_PW_enable(combi_pw_list1)
 pw_verify(4, "% Your password must contain a minimum of 9 characters included with at least")
 
-test_name = "TEST1_PW_combi_enable2"
-TEST1_PW_enable(combi_pw_list2)
+
+##########################################################################
+# 조합 미충족
+##########################################################################
+
+test_name = "TEST2_PW_combi_enable2"
+TEST2_PW_enable(combi_pw_list2)
 pw_verify(4, "% Your password must contain a minimum of 9 characters included with at least")
 
-test_name = "TEST1_PW_repeated_enable"
-TEST1_PW_enable(repeated_pw_list)
+
+##########################################################################
+# 동일 문자 반복
+##########################################################################
+
+test_name = "TEST2_PW_repeated_enable"
+TEST2_PW_enable(repeated_pw_list)
 pw_verify(4, "% Passwords should not have the same characters or numbers in succession.")
 
-test_name = "TEST1_PW_consecutive_enable1"
-TEST1_PW_enable(consecutive_pw_list1)
+
+##########################################################################
+# 연속 문자
+##########################################################################
+
+test_name = "TEST2_PW_consecutive_enable1"
+TEST2_PW_enable(consecutive_pw_list1)
 pw_verify(3, "% Passwords should not have the consecutive characters or numbers in succession.")
 
-test_name = "TEST1_PW_consecutive_enable2"
-TEST1_PW_enable(consecutive_pw_list2)
+
+##########################################################################
+# 역순 연속 문자
+##########################################################################
+
+test_name = "TEST2_PW_consecutive_enable2"
+TEST2_PW_enable(consecutive_pw_list2)
 pw_verify(3, "% Passwords should not have the consecutive characters or numbers in succession.")
 
+
+###################################
+# admin2 계정 삭제
+###################################
+
+device.delete_user(crt, "admin2")
+
+
+##########################################################################
+# 로그아웃 + Console 종료
+##########################################################################
+
+device.close(crt)
--- TEST2_pwcombi_user.py
+++ TEST2_pwcombi_user.py
@@ -1,105 +1,29 @@
+
+### python 3.13.2
+### This script is based on equipment of REALTAK Chipset ###
+
 import time
-from openpyxl import load_workbook
+import sys
 
+sys.path.append(r"C:\Git\script_security_project")
 
-# 결과 파일
-file_path = r"C:\Git\script_security_project\RESULT\securitytest_result.xlsx"
-save_path = file_path
+import device
+import result
 
-# 장비 정보
-device = "SWITCH"
-console = "/SERIAL COM6 /BAUD 115200"
-ip = "192.168.100.22"
-
+###################################
+# 시험 계정 정보
+###################################
 
 username = "admin"
 privilege = "admin"
+
 same_user = [username]
 
 
-
-###################################코드 시작
-
-wb = load_workbook(file_path)
-sheet1 = wb["RESULT"]
-sheet2 = wb["LOG"]
-
-def get_next_row(sheet):
-    for row in range(sheet.max_row, 0, -1):
-        if any(cell.value for cell in sheet[row]):
-            return row +1
-    return 1
-
-
-###전체 line 읽기###
-all_lines = []
-
-def read_all():
-
-    global all_lines
-    all_lines = []
-
-    num_rows = 300
-    num_cols = 300
-
-    for row in range(1, num_rows + 1):
-        line = crt.Screen.Get(row, 1, row, num_cols).rstrip()
-        if line.strip() != "":
-            all_lines.append(line)
-
-
-###시험부분 line만 추출###
-def select(all_lines, TEST_start, TEST_end):
-
-    final_line = []
-    capture = False
-
-    for line in all_lines:
-        if TEST_start in line:
-            capture = True
-            final_line.append(line)
-            continue
-
-        if capture:
-            final_line.append(line)
-            if TEST_end in line:
-                break
-
-    final = "\n".join(final_line)
-
-    return final
-
-
-###판단 부분###
-def pw_verify(judge_count, find_line):
-
-    read_all()
-    final = select(all_lines, f"{device}# {test_name}_start", f"{device}# {test_name}_end")
-    j = 0
-
-    for judge in final.splitlines():
-        if find_line in judge:
-            j += 1
-
-    result = "PASS" if j == judge_count else "FAIL"
-
-    row1 = get_next_row(sheet1)
-    row2 = get_next_row(sheet2)
-
-    sheet1.cell(row=row1, column=2, value=test_name)
-    sheet1.cell(row=row1, column=3, value=result)
-    sheet1.cell(row=row1, column=4, value = j)
-
-    sheet2.cell(row=row2, column=2, value=test_name)
-    sheet2.cell(row=row2, column=3, value=final)
-    
-    wb.save(save_path)
-
 ##########################################################################
-###TEST1 : 1.2.1 비밀번호 보안성 기준###
+# TEST1 : 1.2.1 비밀번호 보안성 기준
+##########################################################################
 
-
-judge_count = 0
 combi_pw_list1 = [
         "024681357A",
         "ADGJLQETUa",
@@ -134,92 +58,152 @@ consecutive_pw_list2 = [
     ]
 
 
-def start():
+###################################
+# Username Password 입력
+###################################
 
-    crt.Session.Connect(console)
-    time.sleep(1)
-    crt.Screen.Send("\n")
-    crt.Screen.Send("admin\n")
-    time.sleep(1)
-    crt.Screen.Send("Changeme1357!\n")
-    crt.Screen.WaitForString(f"{device}>")
-    crt.Screen.Send("enable\n")    
+def make_user():
 
-    
-###config mode 진입###
-def config_mode():
-
-    crt.Screen.Send("conf t\n")
-    crt.Screen.WaitForString(f"{device}(config)# ")
-
-
-#username test 만들기
-def make_username():
-    crt.Screen.Synchronous = True
-
-    #crt.Screen.Send("username test privilege 3 password")
-    crt.Screen.Send(f"username {username} password {privilege}")
-    time.sleep(1)
-    crt.Screen.Send("\r")
-    #crt.Screen.WaitForString("Password")
-    ### HS5412 admin 계정 사용
+    crt.Screen.Send(f"username {username} password {privilege}\n")
     crt.Screen.WaitForString("Enter admin account password")
-    time.sleep(0.5)
-
-    crt.Screen.Synchronous = False
-
-
-def TEST1_PW_user(pw_case):
     
+
+
+###################################
+# Username Password 조합 시험
+###################################
+
+def TEST2_PW_user(pw_case):
+
     crt.Screen.Send(f"{test_name}_start\n")
 
     time.sleep(1)
-    config_mode()
+
+    device.config_mode(crt)
 
     for pw in pw_case:
-        make_username()
-        crt.Screen.Send(f"{pw}\r")
-        time.sleep(0.3)
-        crt.Screen.Send("\n")
-        crt.Screen.WaitForString(f"{device}(config)# ")
 
-    # crt.Screen.Send("\n")
+        make_user()
+
+        crt.Screen.Send(pw)
+        time.sleep(1)
+        crt.Screen.Send("\r")
+
+        crt.Screen.WaitForString(f"{device.device}(config)#")
+
     time.sleep(1)
-    crt.Screen.Send("end\n")
-    time.sleep(0.5)
-    #crt.Screen.Send("show running-config |include test\n")
+
+    device.end_config(crt)
+
     crt.Screen.Send(f"show running-config |include {username}\n")
-    time.sleep(1)
-    crt.Screen.Send("\n")
-    #crt.Screen.Send("show username\n")
+    crt.Screen.WaitForString(f"{device.device}#")
     crt.Screen.Send(f"{test_name}_end\n")
+
     time.sleep(1)
 
 
+###################################
+# 판정
+###################################
+
+def pw_verify(judge_count, find_line):
+
+    final = result.test_part(
+        crt,
+        test_name + "_start",
+        test_name + "_end"
+    )
+
+    j = 0
+
+    for judge in final.splitlines():
+
+        if find_line in judge:
+            j += 1
+
+    if j == judge_count:
+        test_result = "PASS"
+
+    else:
+        test_result = "FAIL"
+
+    judge_value = (
+        f"Count={j}, "
+        f"Expected={judge_count}"
+    )
+
+    result.save_result(
+        test_name,
+        test_result,
+        judge_value,
+        final
+    )
 
 
-start()
+##########################################################################
+# 시험 시작
+##########################################################################
 
-test_name = "TEST1_PW_combi_user1"
-TEST1_PW_user(combi_pw_list1)
+device.start(crt)
+
+
+##########################################################################
+# 비밀번호 조합 미충족 1
+##########################################################################
+
+test_name = "TEST2_PW_combi_user1"
+TEST2_PW_user(combi_pw_list1)
 pw_verify(4, "% Your password must contain a minimum of 9 characters")
 
-test_name = "TEST1_PW_combi_user2"
-TEST1_PW_user(combi_pw_list2) 
+
+##########################################################################
+# 비밀번호 조합 미충족 1
+##########################################################################
+
+test_name = "TEST2_PW_combi_user2"
+TEST2_PW_user(combi_pw_list2) 
 pw_verify(4, "% Your password must contain a minimum of 9 characters")
 
-test_name = "TEST1_PW_repeated_user"
-TEST1_PW_user(repeated_pw_list)
+
+##########################################################################
+# 동일 문자 반복
+##########################################################################
+
+test_name = "TEST2_PW_repeated_user"
+TEST2_PW_user(repeated_pw_list)
 pw_verify(4, "% Passwords should not have the same characters or numbers in succession.")
 
-test_name = "TEST1_PW_consecutive_user1"
-TEST1_PW_user(consecutive_pw_list1)
+
+##########################################################################
+# 연속 문자
+##########################################################################
+
+test_name = "TEST2_PW_consecutive_user1"
+TEST2_PW_user(consecutive_pw_list1)
 pw_verify(3, "% Passwords should not have the consecutive characters or numbers in succession.")
 
-test_name = "TEST1_PW_consecutive_user2"
-TEST1_PW_user(consecutive_pw_list2)
+
+##########################################################################
+# 역순 연속 문자
+##########################################################################
+
+test_name = "TEST2_PW_consecutive_user2"
+TEST2_PW_user(consecutive_pw_list2)
 pw_verify(3, "% Passwords should not have the consecutive characters or numbers in succession.")
 
-test_name = "TEST1_PW_same_user"
-TEST1_PW_user(same_user)
+
+##########################################################################
+# Username과 동일한 Password
+##########################################################################
+
+test_name = "TEST2_PW_same_user"
+TEST2_PW_user(same_user)
 pw_verify(1, "% Passwords should not have contain a user name.")
+
+
+###################################
+# 로그아웃 + Console 종료
+###################################
+
+device.close(crt)
+
